dc7decfeea84db1abe109d6776fbac7779a7f068
779505388 <779505388@qq.com>
2019-01-11 19:38:49 +0800

    2019.1.11 #1

--- py_study/练习.py
+++ py_study/练习.py
@@ -1,4 +1,23 @@
-for i in range(1, 10):
-    for j in range(i, i+1):
-        print('{}*{}={:2}'.format(j, i, i*j), end='')
-    print('')
+'''回文数判断'''
+mun =''
+while type(mun) != type(123):
+
+    try:
+        mun=eval(input('请随机输入一个数：'))
+    except:
+
+        print('请输入一个数！！！')
+
+    
+mun = str(mun)
+sj = ''
+a=99999
+a=len(mun)
+while a >=1:
+    a -=1
+    sj = sj +mun[a]
+    
+if mun == sj:
+    print('输入的数为回文数')
+else:
+    print('输入的不是回文数')
