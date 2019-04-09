a34a3a3e71e84a84f02919126ece792b12f25a5a
779505388 <779505388@qq.com>
2019-04-09 12:38:13 +0800

    2019 4.9

--- 2.py
+++ 2.py
@@ -1,8 +1,26 @@
 import requests
 from lxml import etree
-def get_html(ip='197.25.55.11'):
-    url='https://ip.cn/index.php?ip=%s' % ip
-    html=requests.get(url)
-    page=etree.HTML(html.text)
-    return page.xpath('/html/body/div/div[4]/div/p[2]/code/text()')
-print(get_html('197.25.5.10'))
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6756.400 QQBrowser/10.3.2473.400'}
+
+def ip_port(pages = 3):
+    """获取ip数据,默认爬三页"""
+    https_list= []
+    http_list = []
+    for page in range(1,pages + 1):
+        url= "http://www.ip3366.net/?stype=1&page=%s" % str(page)
+        Html_data = requests.get(url,headers = header)
+        html = etree.HTML(Html_data.text)
+        ip_adrs = html.xpath('//tr/td[1]/text()')
+        ports= html.xpath('//tr/td[2]/text()')
+        ip_types = html.xpath('//tr/td[4]/text()')
+        for ip_adr,port,ip_type in zip(ip_adrs,ports,ip_types):
+            if ip_type == 'HTTPS':
+                ip ='https://' + ip_adr+':'+port
+                https_list.append(ip)
+            else:
+                 ip ='http://' + ip_adr+':'+port
+                 http_list.append(ip)
+    return http_list,https_list
+
+def 
--- py_study/study.py
+++ py_study/study.py
@@ -1,9 +1,72 @@
-'''python递归的学习'''
-def hanoi(n=4,x='x',y='y',z='z'):
-    if n == 1 :
-        print(x,'移动到',z)
+"""ip代理池"""
+import requests
+from lxml import etree
+import pymysql
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6756.400 QQBrowser/10.3.2473.400'}
+
+
+def ip_port(pages=3):
+    """获取ip数据,默认爬三页"""
+    https_list = []
+    http_list = []
+    for page in range(1, pages + 1):
+        url = "http://www.ip3366.net/?stype=1&page=%s" % str(page)
+        Html_data = requests.get(url, headers=header)
+        html = etree.HTML(Html_data.text)
+        ip_adrs = html.xpath('//tr/td[1]/text()')
+        ports = html.xpath('//tr/td[2]/text()')
+        ip_types = html.xpath('//tr/td[4]/text()')
+        for ip_adr, port, ip_type in zip(ip_adrs, ports, ip_types):
+            if ip_type == 'HTTPS':
+                ip = 'https://' + ip_adr+':'+port
+                https_list.append(ip)
+            else:
+                ip = 'http://' + ip_adr+':'+port
+                http_list.append(ip)
+    return http_list, https_list
+
+
+def test_ip(http, htpps):
+    https_list = []
+    http_list = []
+    for test_http in http:
+        try:
+            proxies = {'http': test_http}
+            data = requests.get('http://www.baidu.com/',
+                                headers=header, proxies=proxies, timeout=10)
+            http_list.append(test_http)
+            print('成功:', test_http)
+        except:
+            print('失败')
+
+    for test_https in https:
+        try:
+            proxies = {'https': test_http}
+            data = requests.get('https://www.gynl.xyz/',
+                                headers=header, proxies=proxies, timeout=10)
+            http_list.append(test_https)
+            print('成功:', test_https)
+        except:
+            print('失败')
+    return http_list, https_list
+
+
+def input_ip(http):
+    db = pymysql.connect(host="localhost", user="root",
+                         password="xxxxxxxxxx", db="ip_data", port=3307)
+    cursor = db.cursor()
+
+    sql = """INSERT INTO httpslist(ip) VALUES ('%s')""" % http
+    try:
+        # 执行sql语句
+        cursor.execute(sql)
+        # 提交到数据库执行
+        db.commit()
+    except:
+                    # 如果发生错误则回滚
+        db.rollback()
     else:
-        hanoi(n-1,x,z,y)
-        print(x,'移动到',z)
-        hanoi(n-1,y,x,z)
-hanoi(n=3)
+        print('不存入http')
+
+    db.close()
