5e2f7f9278744faded3e6e93379df1c2e626fed9
779505388 <779505388@qq.com>
2019-04-10 16:42:36 +0800

    2019.4.10

--- py_study/study.py
+++ py_study/study.py
@@ -1,72 +1,19 @@
-"""ip代理池"""
-import requests
-from lxml import etree
-import pymysql
-header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6756.400 QQBrowser/10.3.2473.400'}
-
-
-def ip_port(pages=3):
-    """获取ip数据,默认爬三页"""
-    https_list = []
-    http_list = []
-    for page in range(1, pages + 1):
-        url = "http://www.ip3366.net/?stype=1&page=%s" % str(page)
-        Html_data = requests.get(url, headers=header)
-        html = etree.HTML(Html_data.text)
-        ip_adrs = html.xpath('//tr/td[1]/text()')
-        ports = html.xpath('//tr/td[2]/text()')
-        ip_types = html.xpath('//tr/td[4]/text()')
-        for ip_adr, port, ip_type in zip(ip_adrs, ports, ip_types):
-            if ip_type == 'HTTPS':
-                ip = 'https://' + ip_adr+':'+port
-                https_list.append(ip)
-            else:
-                ip = 'http://' + ip_adr+':'+port
-                http_list.append(ip)
-    return http_list, https_list
-
-
-def test_ip(http, htpps):
-    https_list = []
-    http_list = []
-    for test_http in http:
-        try:
-            proxies = {'http': test_http}
-            data = requests.get('http://www.baidu.com/',
-                                headers=header, proxies=proxies, timeout=10)
-            http_list.append(test_http)
-            print('成功:', test_http)
-        except:
-            print('失败')
-
-    for test_https in https:
-        try:
-            proxies = {'https': test_http}
-            data = requests.get('https://www.gynl.xyz/',
-                                headers=header, proxies=proxies, timeout=10)
-            http_list.append(test_https)
-            print('成功:', test_https)
-        except:
-            print('失败')
-    return http_list, https_list
-
-
-def input_ip(http):
-    db = pymysql.connect(host="localhost", user="root",
-                         password="xxxxxxxxxx", db="ip_data", port=3307)
-    cursor = db.cursor()
-
-    sql = """INSERT INTO httpslist(ip) VALUES ('%s')""" % http
-    try:
-        # 执行sql语句
-        cursor.execute(sql)
-        # 提交到数据库执行
-        db.commit()
-    except:
-                    # 如果发生错误则回滚
-        db.rollback()
-    else:
-        print('不存入http')
-
-    db.close()
+"""GUI"""
+import tkinter as tk
+root = tk.Tk()
+tk.Label(root,text = '账号：').grid(row=0,column=0)
+tk.Label(root ,text = '密码：').grid(row=1,column=0)
+v1=tk.StringVar()
+v2=tk.StringVar()
+e1=tk.Entry(root,textvariable=v1)
+e1.grid(row=0,column=1,padx=10,pady=5)
+e2=tk.Entry(root,textvariable=v2,show='*')
+e2.grid(row=1,column=1,padx=10,pady=5)
+def show():
+    print("作品：《%s》" % e1.get())
+    print("作者：%s" % e2.get())
+tk.Button(root,text='登录',width=10,command=show).grid(row=2,column=0,\
+    padx=10,sticky='W')
+tk.Button(root,text='退出',width=10,command=root.quit).grid(row=2,column=1,\
+    padx = 10,sticky='E')
+tk.mainloop()
