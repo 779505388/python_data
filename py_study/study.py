ce20adcc7e755c5360378fc153cc2678f2d4829d
779505388 <779505388@qq.com>
2019-04-10 17:05:28 +0800

    2019.4.10 #2

--- py_study/study.py
+++ py_study/study.py
@@ -1,19 +1,12 @@
 """GUI"""
 import tkinter as tk
-root = tk.Tk()
-tk.Label(root,text = '账号：').grid(row=0,column=0)
-tk.Label(root ,text = '密码：').grid(row=1,column=0)
-v1=tk.StringVar()
-v2=tk.StringVar()
-e1=tk.Entry(root,textvariable=v1)
-e1.grid(row=0,column=1,padx=10,pady=5)
-e2=tk.Entry(root,textvariable=v2,show='*')
-e2.grid(row=1,column=1,padx=10,pady=5)
-def show():
-    print("作品：《%s》" % e1.get())
-    print("作者：%s" % e2.get())
-tk.Button(root,text='登录',width=10,command=show).grid(row=2,column=0,\
-    padx=10,sticky='W')
-tk.Button(root,text='退出',width=10,command=root.quit).grid(row=2,column=1,\
-    padx = 10,sticky='E')
+master = tk.Tk()
+theLB= tk.Listbox(master,selectmode= 'extended',height =11)
+theLB.pack()
+for item in range(11):
+    theLB.insert('end',item)
+
+theButton = tk.Button(master,text= 'delete',\
+    command = lambda x= theLB:x.delete('active'))
+theButton.pack()
 tk.mainloop()
